d72dbb7d9a26a0ba4ad0000d2af6eaff38d78281
Andres <andres@localhost.localdomain>
2018-03-17 17:03:54 -0300

    metrics

--- src/main/java/sistemac1/SyncEndpoint.java
+++ src/main/java/sistemac1/SyncEndpoint.java
@@ -1,9 +1,17 @@
 package sistemac1;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 @Path("receive")
@@ -12,6 +20,36 @@ public class SyncEndpoint {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void receive(String data) {
+		System.out.println("-----------------------------------");
+		JSONArray jsonarray = new JSONArray(data);
+		System.out.println("Ordenes finales: "+jsonarray.length());
+		
+		JSONObject primeraOrden = jsonarray.getJSONObject(0);
+		System.out.println("Ordenes totales: "+primeraOrden.getString("orderset"));
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		
+		try {
+			
+			Date orderCreationDate = dateFormat.parse(primeraOrden.getString("date"));
+			Date now = new Date();
+			
+			long timelapsed = (now.getTime() - orderCreationDate.getTime())/1000;
+			System.out.println("Tiempo de flujo de SI: "+timelapsed+ "ms");
+			
+			long timeRateSubtotal = jsonarray.length()/timelapsed;
+			System.out.println("Ordenes finales/Tiempo: "+timeRateSubtotal+ " ordenes/s");
+
+			long timeRateTotal = Integer.parseInt(primeraOrden.getString("orderset"))/timelapsed;
+			System.out.println("Ordenes totales/Tiempo: "+timeRateTotal+ " ordenes/s");
+			
+			
+		} catch (JSONException | ParseException e) {			
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("-----------------------------------");
 		
 		System.out.println(data);
 		
